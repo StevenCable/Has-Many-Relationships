aaada80d467a563db7a842d2abc7f9bf8818828d
Steven Cable <steven.cable12@gmail.com>
2017-01-24 22:11:41 -1000

    created tables user, post, comments working on relationship table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -11,4 +11,19 @@ CREATE TABLE users (
   id SERIAL NOT NULL PRIMARY KEY, username varchar(90) NOT NULL, first_name varchar(90) NULL, last_name varchar(90) NULL, created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
+DROP TABLE IF EXISTS "posts";
+CREATE TABLE posts (
+  id SERIAL NOT NULL PRIMARY KEY, title varchar(180) NULL, url varchar(510) NULL, content text NULL, created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+DROP TABLE IF EXISTS "comments";
+CREATE TABLE comments (
+  id SERIAL NOT NULL PRIMARY KEY, body varchar(510) NULL, created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+
+
+
 \d+ "users";
+\d+ "posts";
+\d+ "comments";
